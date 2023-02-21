03f7d5abd38d5943b6100dcb15426ad7b2fe787b
Xero1000 <alex.heil@comcast.net>
2023-02-21 15:47:13 -0500

    Week 3 Day 1 if statement and for loop assignments

---
+++ week3/LectureDay1/Day 1 HW/if-statements.js
@@ -0,0 +1,19 @@
+//Challenge 1
+let cups = 3;
+
+if (cups < 3){
+    console.log("Yes I'll take another cup of coffee.");
+}
+else {
+    console.log("I think I'm okay for now.");
+}
+
+// Challenge 2
+let miles = 500;
+
+if (miles > 500) {
+    console.log("Yes I think I need an oil change.");
+}
+else{
+    console.log("My car doesn't need an oil change yet.");
+}
