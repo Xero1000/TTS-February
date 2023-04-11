ba20ef946c3e83062e77aa07edb7a096c310f4cc
Xero1000 <alex.heil@comcast.net>
2023-04-11 17:13:48 -0400

    Mars JSON response example file

---
+++ week9/LectureDay3/mars.js
@@ -0,0 +1,24 @@
+
+// JSON response example with information about the planet Mars
+
+// {
+//     "planetNumber" : 4,
+//     "averageTemperature" : {
+//         "fahrenheight" : -80,
+//         "celsius" : -60
+//     },
+//     "atmosphere" : {
+//         "composition" : {
+//             "carbonDioxide" : "95%",
+//             "nitrogen" : "2.8%", 
+//             "argon" : "2%",
+//             "oxygen" : ".174%",
+//             "carbonMonoxide" : "0.0747%"
+//         },
+//         "mass" : "2.5x1016 kg",
+//         "averageSurfacePressure" : "610 Pa"
+//     },
+//     "diameterInMiles" : 4212.3,
+//     "numberOfMoons" : 2,
+//     "moons": ["Phobos", "Deimos"]
+// }
