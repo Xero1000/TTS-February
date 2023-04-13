175a1742b169e68101f3ff0e54726c8d2b6ca05b
Xero1000 <alex.heil@comcast.net>
2023-04-13 18:14:17 -0400

    Initial Commit with h2 console enabled in properties

---
+++ week11/FavoriteRestaurantsList/src/main/java/com/techtalentsouth/FavoriteRestaurantsList/FavoriteRestaurantsListApplication.java
@@ -0,0 +1,13 @@
+package com.techtalentsouth.FavoriteRestaurantsList;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FavoriteRestaurantsListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FavoriteRestaurantsListApplication.class, args);
+	}
+
+}
---
+++ week11/FavoriteRestaurantsList/src/test/java/com/techtalentsouth/FavoriteRestaurantsList/FavoriteRestaurantsListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techtalentsouth.FavoriteRestaurantsList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FavoriteRestaurantsListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
