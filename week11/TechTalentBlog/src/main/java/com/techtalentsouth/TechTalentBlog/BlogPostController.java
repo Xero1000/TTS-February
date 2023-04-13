4ee31f5241e97d6ed98821a32a6f4da7a885fc80
Xero1000 <alex.heil@comcast.net>
2023-04-13 18:07:42 -0400

    TTS Blog Project 2 Slide 20 (to 40) through TTS Blog Project 3 Slide 5

--- week11/TechTalentBlog/src/main/java/com/techtalentsouth/TechTalentBlog/BlogPostController.java
+++ week11/TechTalentBlog/src/main/java/com/techtalentsouth/TechTalentBlog/BlogPostController.java
@@ -7,8 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class BlogPostController {
@@ -20,6 +22,10 @@ public class BlogPostController {
 	
 	@GetMapping(value="/")
     public String index(BlogPost blogPost, Model model) {
+	    posts.removeAll(posts);	
+	    for (BlogPost post : blogPostRepository.findAll()) {		
+	    	posts.add(post);		
+	    }
 		model.addAttribute("posts", posts);
 		System.out.println(model);
     	return "blogpost/index";
@@ -34,7 +40,6 @@ public class BlogPostController {
 	@PostMapping(value = "/blogposts")
     public String addNewBlogPost(BlogPost blogPost, Model model) {
 		blogPostRepository.save(blogPost);
-		posts.add(blogPost);
 		model.addAttribute("title", blogPost.getTitle());
 		model.addAttribute("author", blogPost.getAuthor());
 		model.addAttribute("blogEntry", blogPost.getBlogEntry());
@@ -42,4 +47,11 @@ public class BlogPostController {
 		System.out.println(model);
 		return "blogpost/result";
     }
+	
+	@RequestMapping(value = "/blogposts/{id}", method = RequestMethod.DELETE)
+	public String deletePostWithId(@PathVariable Long id, BlogPost blogPost) {
+
+	    blogPostRepository.deleteById(id);
+	    return "blogpost/index";
+	}
 }
