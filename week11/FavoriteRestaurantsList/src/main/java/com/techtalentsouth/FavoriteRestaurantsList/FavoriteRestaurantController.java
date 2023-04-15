c6548ea86b97c1a4866c9f4e60bf23b4a2afec11
Xero1000 <alex.heil@comcast.net>
2023-04-15 12:02:29 -0400

    Added Editing functionality

--- week11/FavoriteRestaurantsList/src/main/java/com/techtalentsouth/FavoriteRestaurantsList/FavoriteRestaurantController.java
+++ week11/FavoriteRestaurantsList/src/main/java/com/techtalentsouth/FavoriteRestaurantsList/FavoriteRestaurantController.java
@@ -2,6 +2,7 @@ package com.techtalentsouth.FavoriteRestaurantsList;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -42,9 +43,36 @@ public class FavoriteRestaurantController {
 		return "favoriteRestaurant/result";
 	}
 	
-	@GetMapping(path="/deletePage/{id}")
+	@GetMapping(value="/deletePage/{id}")
 	public String deleteEntry(@PathVariable Long id) {
 		restaurantRepository.deleteById(id);
 		return "favoriteRestaurant/delete";
+	}
+	
+	@GetMapping(value="/editPage/{id}")
+	public String editEntry(@PathVariable Long id, Restaurant restaurant, Model model) {
+		Optional<Restaurant> entry = restaurantRepository.findById(id);
+		if (entry.isPresent()) {
+			Restaurant actualEntry = entry.get();
+			model.addAttribute("restaurant", actualEntry);
+			model.addAttribute("id", actualEntry.getId());
+			model.addAttribute("name", actualEntry.getName());
+			model.addAttribute("websiteLink", actualEntry.getWebsiteLink());
+			model.addAttribute("pictureLink", actualEntry.getPictureLink());
+		}
+		return "favoriteRestaurant/edit";
+	}
+	
+	@PostMapping(value="/editPage/{id}")
+	public String updateEntry(@PathVariable Long id, Restaurant restaurant, Model model) {
+		Optional<Restaurant> entry = restaurantRepository.findById(id);
+		if (entry.isPresent()) {
+			Restaurant actualEntry = entry.get();
+			actualEntry.setName(restaurant.getName());
+			actualEntry.setWebsiteLink(restaurant.getWebsiteLink());
+			actualEntry.setPictureLink(restaurant.getPictureLink());
+			restaurantRepository.save(actualEntry);
+		}
+		return "favoriteRestaurant/result";
 	}
 }
